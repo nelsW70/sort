c97ef8d2f2d0ec8081fe399ecbc059ff85cc29ff
Nelson Foltz <nelsfoltz@gmail.com>
2020-05-25 13:37:58 -0400

    add: characterscollection class for sorting strings

--- build/index.js
+++ build/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollections_1 = require("./NumbersCollections");
-var numbersCollection = new NumbersCollections_1.NumbersCollection([10, 3, -5, 0]);
+var numbersCollection = new NumbersCollections_1.NumbersCollection([50, 3, -5, 0]);
 var sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
