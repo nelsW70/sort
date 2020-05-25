02c93d6bb2dc02759fa0df1249ed79c6e32a12a1
Nelson Foltz <nelsfoltz@gmail.com>
2020-05-25 11:09:08 -0400

    add: using typeguards to demonstrate the wrong way to do it

--- build/index.js
+++ build/index.js
@@ -7,10 +7,16 @@ var Sorter = /** @class */ (function () {
         var length = this.collection.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+                // if collections is numbers[]
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                // all of this only works if collection is a string[]
+                if (typeof this.collection === 'string') {
                 }
             }
         }
